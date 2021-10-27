7e47d5aa41bd2856fd91f12b935aed866a5a3fbe
Johan Wintgens <johan.wintgens@hotmail.com>
2021-10-27 17:58:43 +0200

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# WindowsGSM.Satisfactory
+🧩WindowsGSM plugin that provides Satisfactory Dedicated server support!
+
+# The Game
+https://store.steampowered.com/app/526870/Satisfactory/
+
+# Requirements
+WindowsGSM >= 1.21.0
+
+# Save Files
+The save files are located in the host folder %LOCALAPPDATA%\FactoryGame\Saved\Savegames\server
+
+# Config
+After starting the server, the first client to connect to the server will be able to configure it ingame.
+
+# Installation
+  1. Download the latest release
+  2. Move Satisfactory.cs folder to plugins folder
+  3. Click [RELOAD PLUGINS] button or restart WindowsGSM
+
+# Known Issues
+FactoryServer.exe launches a new window by itself. This means that FactoryServer's window can be hidden, but the window it spawns cannot. Seeing as the dedicated server was released only 24 hours ago at this time, I am confident that this will be fixed internally.
+
+# License
+This project is licensed under the MIT License - see the <a href="https://github.com/johanwintgens/WindowsGSM.Satisfactory/blob/main/LICENSE">LICENSE.md</a> file for details
+
+# Credits
+This repository and project is essentially a copy of <a href="dkdue">https://github.com/dkdue</a>'s <a href="https://github.com/dkdue/WindowsGSM.Valheim">WindowsGSM.Valheim</a> project, tailored to Satisfactory's requirements.
---
+++ Satisfactory.cs/Satisfactory.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Query;
+using WindowsGSM.GameServer.Engine;
+
+namespace WindowsGSM.Plugins
+{
+    public class Satisfactory : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.Satisfactory",
+            author = "johanwintgens",
+            description = "WindowsGSM plugin for supporting Satisfactory Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/johanwintgens/WindowsGSM.Satisfactory",
+            color = "#ffffff" // Color Hex
+        };
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "1690800"; // Game server appId
+
+        // - Standard Constructor and properties
+        public Satisfactory(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+
+
+        // - Game server Fixed variables
+        public override string StartPath => @"FactoryServer.exe"; // Game server start path
+        public string FullName = "Satisfactory Dedicated Server"; // Game server FullName
+        public bool AllowsEmbedConsole = true;
+        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        // - Game server default values
+        public string Port = "7777"; // Default port
+        public string QueryPort = "157777"; // Default query port
+        public string Defaultmap = "Dedicated"; // Default map name
+        public string Maxplayers = "10"; // Default maxplayers
+        public string Additional = "-log -unattended"; // Additional server start parameter
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            // Satisfy log
+            await Task.CompletedTask;
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            // Satisfy log
+            await Task.CompletedTask;
+
+            string fileName = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+
+            if (!File.Exists(fileName))
+            {
+                Error = $"{Path.GetFileName(fileName)} not found ({fileName})";
+                return null;
+            }
+
+            // Prepare Process
+            var p = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo =
+                {
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = fileName,
+                    Arguments = Additional,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+            }
+            catch (Exception e)
+            {
+                // Return null upon crashing
+                Error = e.Message;
+                return null;
+            }
+
+            if (AllowsEmbedConsole)
+            {
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+            }
+
+            return p;
+        }
+
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
+            });
+
+            await Task.Delay(2000);
+        }
+    }
+}
